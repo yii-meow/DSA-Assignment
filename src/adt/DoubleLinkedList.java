aa8a14762e70e9ae57d46635caf9a400170235b4
yii-meow <yiksoonchong2002@gmail.com>
2023-08-27 16:55:28 +0800

    update adt

--- src/adt/DoubleLinkedList.java
+++ src/adt/DoubleLinkedList.java
@@ -4,19 +4,44 @@
  */
 package adt;
 
+import java.io.Serializable;
+
 /**
  *
  * @author yikso
  */
-public class DoubleLinkedList<T> implements ListInterface<T> {
+public class DoubleLinkedList<T> implements ListInterface<T>, Serializable {
+
+    private int numberOfEntries;
+    private Node firstNode;
+
+    public DoubleLinkedList() {
+        clear();
+    }
 
     @Override
     public boolean add(T newEntry) {
+        Node newNode = new Node(newEntry);
+
+        if (numberOfEntries == 0) {
+            firstNode = newNode;
+        } else {
+            Node currentNode = null;
+            while (firstNode.next != null) {
+                currentNode = firstNode.next;
+            }
+            currentNode.next = newNode;
+        }
+
         return true;
     }
 
     @Override
     public boolean add(int position, T newEntry) {
+        if (position >= 1 || position < numberOfEntries + 1) {
+
+        }
+
         return true;
     }
 
@@ -27,7 +52,8 @@ public class DoubleLinkedList<T> implements ListInterface<T> {
 
     @Override
     public void clear() {
-        
+        firstNode = null;
+        numberOfEntries = 0;
     }
 
     @Override
@@ -37,12 +63,12 @@ public class DoubleLinkedList<T> implements ListInterface<T> {
 
     @Override
     public int getNumberOfEntries() {
-        return 0;
+        return numberOfEntries;
     }
 
     @Override
     public boolean isEmpty() {
-        return true;
+        return numberOfEntries == 0;
     }
 
     @Override
@@ -54,15 +80,22 @@ public class DoubleLinkedList<T> implements ListInterface<T> {
 
         private T data;
         private Node next;
+        private Node prev;
 
         private Node(T data) {
             this.data = data;
             this.next = null;
+            this.prev = null;
         }
 
         private Node(T data, Node next) {
             this.data = data;
             this.next = next;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
 }
