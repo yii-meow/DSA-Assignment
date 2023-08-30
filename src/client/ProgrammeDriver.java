de2d2c2050ce7d2fb582acca7fb742d318a86e4a
yiyi_meow <80099417+yii-meow@users.noreply.github.com>
2023-08-30 14:09:02 +0800

    Update ProgrammeDriver.java

--- src/client/ProgrammeDriver.java
+++ src/client/ProgrammeDriver.java
@@ -7,6 +7,7 @@ package client;
 import adt.DoubleLinkedList;
 import adt.ListInterface;
 import entity.Programme;
+import java.util.Scanner;
 
 /**
  *
@@ -21,16 +22,95 @@ public class ProgrammeDriver {
     }
 
     public static void menu() {
-        System.out.println("======================\n Programme Management\n======================");
-        System.out.println("1. Add a new programme");
-        System.out.println("2. Remove a new programme");
-        System.out.println("3. Find programme");
-        System.out.println("4. Amend programme details");
-        System.out.println("5. List all programmes");
-        System.out.println("6. Add a tutorial group to a programme");
-        System.out.println("7. Remove a tutorial group from a programme");
-        System.out.println("8. List all tutorial groups from a programme");
-        System.out.println("9. Generate relevant reports");
+
+        int choice = 0;
+
+        do {
+            System.out.println("======================\n Programme Management\n======================");
+            System.out.println("1. Add a new programme");
+            System.out.println("2. Remove a programme");
+            System.out.println("3. Find programme");
+            System.out.println("4. Amend programme details");
+            System.out.println("5. List all programmes");
+            System.out.println("6. Add a tutorial group to a programme");
+            System.out.println("7. Remove a tutorial group from a programme");
+            System.out.println("8. List all tutorial groups from a programme");
+            System.out.println("9. Generate relevant reports\n\n");
+
+            System.out.print("Choice: ");
+
+            Scanner scanner = new Scanner(System.in);
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addProgramme();
+                    break;
+                case 2:
+                    removeProgramme();
+                    break;
+                case 3:
+                    findProgramme();
+                    break;
+                case 4:
+                    amendProgramme();
+                    break;
+                case 5:
+                    listProgramme();
+                    break;
+                case 6:
+                    addGroupToProgramme();
+                    break;
+                case 7:
+                    removeGroupFromProgramme();
+                    break;
+                case 8:
+                    listGroupFromProgramme();
+                    break;
+                case 9:
+                    generateReport();
+                    break;
+                default:
+                    System.out.println("Thank you for using this system.");
+            }
+
+        } while (choice >= 0 && choice <= 9);
     }
 
+    private static void addProgramme() {
+
+    }
+
+    private static void removeProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void findProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void amendProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void listProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void addGroupToProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void removeGroupFromProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void listGroupFromProgramme() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void generateReport() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
