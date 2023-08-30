8ba76a2f3a8f21a05fd4c4c02fafa86260547192
yii-meow <yiksoonchong2002@gmail.com>
2023-08-30 13:08:39 +0800

    client ui

--- src/client/ProgrammeDriver.java
+++ src/client/ProgrammeDriver.java
@@ -14,9 +14,23 @@ import entity.Programme;
  */
 public class ProgrammeDriver {
 
+    ListInterface<Programme> programme = new DoubleLinkedList<>();
+
     public static void main(String[] args) {
-        ListInterface<Programme> programme = new DoubleLinkedList<>();
-        programme.add(new Programme(""));
+        menu();
+    }
+
+    public static void menu() {
+        System.out.println("======================\n Programme Management\n======================");
+        System.out.println("1. Add a new programme");
+        System.out.println("2. Remove a new programme");
+        System.out.println("3. Find programme");
+        System.out.println("4. Amend programme details");
+        System.out.println("5. List all programmes");
+        System.out.println("6. Add a tutorial group to a programme");
+        System.out.println("7. Remove a tutorial group from a programme");
+        System.out.println("8. List all tutorial groups from a programme");
+        System.out.println("9. Generate relevant reports");
     }
 
 }
